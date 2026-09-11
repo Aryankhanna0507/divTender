1e20aaa14d97c94f0629348507218136a33c8215
Aryan <khannaaryan460@gmail.com>
2026-09-12 00:45:37 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -3,60 +3,43 @@ const express=require("express");
 
 const app=express(); 
 
-// REQUEST GET /user=> it will go through the chain of middlewares(function that are before the function having res.send() ) and at the end it send the reqeustion 
-// the last function that having res.send()(means the function that sending response) is the main response or route handler and other are known as the middleware 
-// we can have multiple route hander for the same route (we already know it)
-// they are used of performing several operation before sending the respond like aunthentication , autheraization and many more 
-// but we can create these function in different routes like this-
-// app.use("/user",(req,res,next)=>{
-//   console.log("route from the the first");
-//   next();
-// })
-// app.use("/user",(req,res,next)=>{
-//   console.log("route from the second one");
-//   // next();
-//   res.send("finally got the response from route 2")
-// })
+// error handling
 
-// example-:
-
-// handle the auth mideleware for all GET,POST... requests
-
-// app.use("/user",(req,res,next)=>{
-//   const token="xyz";
-//   const isAuthenticated=token=="xyz";
-//   if(isAuthenticated){
-//     console.log("user is authenticaed");
-//     next();
-//   }else{
-//     res.status(401).send("the user is not authenticated");
-//   }
-// })
-// commenting this because in best practices we create the middleware is seperate folder
-
-// we can also wriete like this 
-app.use("/admin",adminAuth);
-app.get("/admin/getData",(req,res)=>{
-  res.send("giving the data to admin");
+app.use("/",(err,req,res,next)=>{ // it will not do anything because we do not have error here it will only work when you throw error 
+  if(err){
+    // Log your erros
+    res.status(500).send("something went wrong")
+  }
 })
 
+// app.get("/getUserData",(req,res)=>{
+  // //logic of db call and get the user data
+  // // what if code have some errors
+  // throw new Error("dkfoedfj"); // this is not the good way to handle errors
+  
+  // res.send("data has been sent ot the user")
+  // })
+  // always try to write all the code in try catch block
+  // using try catch here
+app.get("/getUserData",(req,res)=>{
+  try{
 
-// before giving the daata first check weather the uer is authenticated or not 
-app.get("/user/getAllData",userAuth,(req,res)=>{
-  res.send("got the complete data");
+    //logic of db call and get the user data
+    // what if code have some errors
+    throw new Error("dkfoedfj"); // this is not the good way to handle errors
+    res.send("data has been sent ot the user")
+  }catch(err){
+    res.status(500).send("some error contact suppot team")
+  }
+  // now the error will be handled inside the catch block it will not go ahead
 })
-// before deleting the data first check weather the user is authenticated or not 
-app.delete("/user/deleteData",userAuth,(req,res)=>{
-  res.send("data has been deleted successfully");
+app.use("/",(err,req,res,next)=>{ // "/" -> means it matches all the routes basically its like a wild card (here it is working as the error handling middleware)
+  if(err){
+    // Log your erros
+    res.status(500).send("something went wrong")
+  }
 })
-app.post("/user/login",(req,res)=>{
-  res.send("user logged in");
-})
-
-
-
-
-
+// always keep this error handling middleware at the end
 app.listen(3000,()=>{
   console.log("your server is listing successfully on port 3000....");
 });
